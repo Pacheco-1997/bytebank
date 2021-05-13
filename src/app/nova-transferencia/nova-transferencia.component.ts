b817025dd86b422941ba06eacd387e5857411abd
Victor Pacheco <vpacheco26@yahoo.com.br>
2021-05-12 22:42:51 -0300

    adicionando componente extrato

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output , EventEmitter} from '@angular/core';
+
 
 @Component({
     selector:'app-nova-transferencia',
@@ -7,10 +8,20 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent{
 
+@Output() aoTransferir = new EventEmitter<any>();
+
 valor: number;
 destino: number;
 
   transferir(){
-    console.log(this.valor + " " + this.destino);
+
+    const valorEmitir = {valor: this.valor,destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = null;
+    this.destino = null;
   }
 }
